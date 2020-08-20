329a675800b3720be74933cc8c12ac30d6ac6b18
Roque Laecio dos Santos <rlaecio@hotmail.com>
2020-08-20 16:46:02 +0100

    bdd behave test

---
+++ aula10/features/steps/steps.py
@@ -0,0 +1,16 @@
+from behave import step
+
+def soma(x, y):
+    return x + y
+
+@step('somar "{n1}" com "{n2}"')
+def test_soma(context, n1, n2):
+    context.r_soma = soma(int(n1), int(n2))
+    return context.r_soma
+
+@step('o resultado deve ser "{esperado}"')
+def test_soma_result(context, esperado):
+    assert context.r_soma == int(esperado), "descrição do erro "
+    
+
+
