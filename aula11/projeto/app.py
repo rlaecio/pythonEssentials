c3884717c111e765ffc98b9d318a42d1d5373bbe
Roque Laecio dos Santos <rlaecio@hotmail.com>
2020-08-21 11:34:19 +0100

    create app

---
+++ aula11/projeto/app.py
@@ -0,0 +1,16 @@
+import modules.banco as banco
+import threading
+
+if __name__ == '__main__':
+    user  = input('Nickname: ')
+    try:
+        f = threading.Thread(target=banco.busca_mensagem)
+        f.start()  
+        
+    except Exception as e:
+        print('Falha ao criar a thread: {} '.format(e))
+        
+    while f.isAlive:
+        mens = input()
+        banco.cadastrar_mensagem(user, mens)
+
--- aula11/projeto/modules/banco.py
+++ aula11/projeto/modules/banco.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient, DESCENDING
 import time
 
 try:
-    con = MongoClient()
+    con = MongoClient('mongodb://192.168.1.9:27017/')
     db = con['chat']
 except Exception as e:
     print('ERRO: {}'.format(e))
@@ -18,10 +18,11 @@ def cadastrar_mensagem(nome, mensagem):
     db.chat.insert(date)
     
 def busca_mensagem():
-    ultimo = [ x for x in db.chat.bind().sort('_id', DESCENDING)]
+    ultimo = [ x for x in db.chat.find().sort('_id', DESCENDING)]
     while True:
-        date = [ x for x in db.chat.bind().sort('_id', DESCENDING)]
+        date = [ x for x in db.chat.find().sort('_id', DESCENDING)]
         if date != ultimo:
+            ultimo = date
             print("[{}] {} : {} \n".format(
                 date[0]['hora'], date[0]['nome'], date[0]['mensagem']
             ))
